7ddcbd61d623dc7295c44a410217ab05cf4c1786
Thomas James Delaney <thomas.delaney@posteo.net>
2019-07-05 10:22:01 +0100

    Added a script.

---
+++ py/load_data.py
@@ -0,0 +1,10 @@
+import os, sys, argparse, glob
+import numpy as np
+import pandas as pd
+
+proj_dir = os.path.join(os.environ['PROJ'], 'Tennant_Project')
+data_dir = os.path.join(proj_dir, 'data', 'python_3')
+
+spatial_firing_frame = pd.read_pickle(os.path.join(data_dir, 'spatial_firing.pkl'))
+processed_position_frame = pd.read_pickle(os.path.join(data_dir, 'processed_position_data.pkl'))
+
